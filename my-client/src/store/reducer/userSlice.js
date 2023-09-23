e11b7cc609b61a8cdb59970fe4f840cf6a3c3a68
Dake Wang <xibuyazai@gmail.com>
2023-09-23 16:50:36 -0700

    use userSlice instead of the way before

--- my-client/src/store/reducer/userSlice.js
+++ my-client/src/store/reducer/userSlice.js
@@ -1,22 +1,56 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
-import { useGetUsersQuery, useGetUserByIdQuery, useUpdateUserMutation, useDeleteUserMutation } from "../api/userApi";
+import {userApi} from "../api/userApi";
 
+// 异步操作：获取所有用户
 export const fetchUsers = createAsyncThunk(
     'users/fetchUsers',
     async (_, { dispatch }) => {
-        const { data } = await useGetUsersQuery().unwrap();
-        return data;
+        const result = await dispatch(userApi.endpoints.getUsers.initiate());
+        if (result.error) {
+            throw result.error;
+        }
+        return result.data;
     }
 );
 
+// 异步操作：根据ID获取用户
 export const fetchUserById = createAsyncThunk(
     'users/fetchUserById',
     async (id, { dispatch }) => {
-        const { data } = await useGetUserByIdQuery(id).unwrap();
-        return data;
+        const result = await dispatch(userApi.endpoints.getUserById.initiate(id));
+        console.log("Response from fetchUserById:", result); // Add this line
+        if (result.error) {
+            throw result.error;
+        }
+        return result.data;
     }
 );
 
+// 异步操作：更新用户
+export const updateUser = createAsyncThunk(
+    'users/updateUser',
+    async (user, { dispatch }) => {
+        const result = await dispatch(userApi.endpoints.updateUser.initiate(user));
+        if (result.error) {
+            throw result.error;
+        }
+        return result.data;
+    }
+);
+
+// 异步操作：删除用户
+export const deleteUser = createAsyncThunk(
+    'users/deleteUser',
+    async (id, { dispatch }) => {
+        const result = await dispatch(userApi.endpoints.deleteUser.initiate(id));
+        if (result.error) {
+            throw result.error;
+        }
+        return id;
+    }
+);
+
+// 定义userSlice
 export const userSlice = createSlice({
     name: 'users',
     initialState: {
@@ -24,24 +58,15 @@ export const userSlice = createSlice({
         selectedUser: null,
         status: 'idle',
     },
-    reducers: {
-        updateUserState(state, action) {
-            const userIndex = state.users.findIndex(user => user.id === action.payload.id);
-            if (userIndex >= 0) {
-                state.users[userIndex] = action.payload;
-            }
-        },
-        deleteUserState(state, action) {
-            const userIndex = state.users.findIndex(user => user.id === action.payload);
-            if (userIndex >= 0) {
-                state.users.splice(userIndex, 1);
-            }
-        },
-    },
+    reducers: {},
     extraReducers: (builder) => {
         builder
             .addCase(fetchUsers.pending, (state) => {
                 state.status = 'loading';
+            })
+            .addCase(fetchUsers.rejected, (state, action) => {
+                state.status = 'error';
+                state.error = action.error.message;
             })
             .addCase(fetchUsers.fulfilled, (state, action) => {
                 state.status = 'idle';
@@ -49,10 +74,21 @@ export const userSlice = createSlice({
             })
             .addCase(fetchUserById.fulfilled, (state, action) => {
                 state.selectedUser = action.payload;
+            })
+            .addCase(updateUser.fulfilled, (state, action) => {
+                const userIndex = state.users.findIndex(user => user.id === action.payload.id);
+                if (userIndex >= 0) {
+                    state.users[userIndex] = action.payload;
+                }
+            })
+            .addCase(deleteUser.fulfilled, (state, action) => {
+                const userIndex = state.users.findIndex(user => user.id === action.payload);
+                if (userIndex >= 0) {
+                    state.users.splice(userIndex, 1);
+                }
             });
     },
 });
 
-export const { updateUserState, deleteUserState } = userSlice.actions;
-
-export default userSlice.reducer;
+// 导出actions
+export const {} = userSlice.actions;
